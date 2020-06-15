0c889cd5a3e3719dbcd4de3c88feef907db2cbeb
sucharitha <sucharitha.chinnam@brightsites.co.uk>
2020-06-15 11:01:47 +0100

    Using mongo environment URI

--- app/config/dev.js
+++ app/config/dev.js
@@ -1,5 +1,5 @@
 
 const config = {
-    mongoURI: "mongodb+srv://sucharitha:Asdf1234@cluster0-yiol3.mongodb.net/couriers?retryWrites=true&w=majority"
+    mongoURI: process.env.MONGO_URI
 }
 module.exports = config
